189ec039424a894927e26e7d01f83a7cb1eb93a1
dannygrimaldi <71056766+dannygrimaldi@users.noreply.github.com>
2023-07-25 20:34:50 -0600

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,22 +1,26 @@
-// mi_script.js
 function loadSelectedTheme() {
   const body = document.body;
   const selectedTheme = localStorage.getItem('selectedTheme');
+  var table = document.querySelector('#albums_by_decade');
 
   if (selectedTheme === 'dark') {
     body.classList.add('bootstrap-dark');
     body.classList.add('dark-theme');
+    table.classList.add('table-dark');
+
   } else {
-    body.classList.add('bootstrap');
+    //body.classList.add('bootstrap');
   }
 }
 
 function toggleTheme() {
   const body = document.body;
+  var table = document.querySelector('#example');
 
-  body.classList.toggle('bootstrap');
+
   body.classList.toggle('bootstrap-dark');
   body.classList.toggle('dark-theme');
+  table.classList.toggle('table-dark');
 
   // Verificar qué tema se ha aplicado y almacenarlo en el localStorage
   if (body.classList.contains('dark-theme')) {
