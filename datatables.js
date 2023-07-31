90eee9d6ada03645c90b594dfcc83ba93d160c80
dannygrimaldi <71056766+dannygrimaldi@users.noreply.github.com>
2023-07-31 16:58:47 -0600

    Update datatables.js

--- datatables.js
+++ datatables.js
@@ -1,24 +1,15 @@
-<!-- Agrega las librerías necesarias -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.jsdelivr.net/momentjs/2.29.1/moment.min.js"></script>
-<script src="https://cdn.datatables.net/plug-ins/1.11.5/sorting/datetime-moment.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <link href="https://nightly.datatables.net/css/jquery.dataTables.css" rel="stylesheet" type="text/css" />
+    <script src="https://nightly.datatables.net/js/jquery.dataTables.js"></script>
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.10.13/dataRender/datetime.js"></script>
 
 
-<!-- Configura tu datatable -->
-<script>
-$(document).ready(function() {
-  $('#miTabla').DataTable({
-    columnDefs: [{
-      targets: 0, // Índice de la columna fecha (0 es la primera columna)
-      type: 'datetime-moment', // Usa el plugin de DataTables Moment para ordenar por fechas
-      render: function (data, type, full, meta) {
-        // Formatea la fecha para que se muestre correctamente
-        return moment(data, 'DD/MM/YYYY').format('DD/MM/YYYY'); // Ajusta el formato según tus necesidades
-      }
-    }],
-    // Resto de configuraciones de tu datatable
-  });
-});
-</script>
+$('#example').DataTable( {
+    columnDefs: [ {
+      targets: 4,
+      render: DataTable.render.datetime( 'YYYY/MM/DD')
+    } ]
+  } );
